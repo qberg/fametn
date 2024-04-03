3528e227f0977aa0255dcc519689d107e31e37ae
Sooryakiran <sooryakiranpallikulathil@gmail.com>
2024-04-03 23:09:28 +0530

    POlaceholder for tncgs

---
+++ src/pages/finance/tncgs/index.tsx
@@ -0,0 +1,12 @@
+import RootLayout from "@/pages/layout";
+import { Container } from "react-bootstrap";
+
+const TNCGS = () => {
+    return (<RootLayout>
+        <Container>
+            hgi
+        </Container>
+    </RootLayout>)
+}
+
+export default TNCGS;
