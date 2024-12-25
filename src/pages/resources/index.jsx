44bcd67590be8a6689b09cf05b2f982772bdd20f
Sooryakiran <sooryakiranpallikulathil@gmail.com>
2024-12-25 18:07:47 +0530

    added resources page

---
+++ src/pages/resources/index.jsx
@@ -0,0 +1,148 @@
+import { Col, Container, Row } from "react-bootstrap";
+import styles from "./resources.module.css";
+import RootLayout from "../../components/layout/layout";
+import YellowPill from "../../components/yellowpill";
+import { CacheHeaders } from "../../utils/definitions";
+import { getDataFromPath, getNewsletterData } from "../../utils/api_calls";
+import { getAllCategories, getAllResourceCategories, getAllResources } from "../../utils/resources";
+import { useState } from "react";
+import DynamicImage from "../../components/dynamicImage";
+import Image from "next/image";
+import Newsletterform from "../../components/newsletterform";
+
+function ImageCard({ data }) {
+    const attachementUrl = process.env.NEXT_PUBLIC_IMG_ENDPOINT + data?.attachment?.data?.attributes?.url;
+
+    const startDownload = () => {
+        window.open(attachementUrl, "_blank");
+    }
+
+    return (
+        <div onClick={startDownload} className={styles.imagecard}>
+            <DynamicImage src={data.image} objectFit="cover" />
+            <div className={styles.imagecardtext}>
+                <div className="d-flex">
+                    <div className="ms-auto">
+                        <div className={styles.downloadbutton}>
+                            <div className="position-relative m-auto">
+                                <Image src={"/resources_download_button.svg"} height={22} width={22} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-auto">
+                    <h4 className={styles.whitetext}>
+                        {data.title}
+                    </h4>
+                    <p className={`small ${styles.whitetext}`}>
+                        {data.description}
+                    </p>
+                    <div>
+                        <div className={styles.yellowcircle}>
+                            <div className="position-relative m-auto">
+                                <Image src={"/resources_arrow_right.svg"} height={15} width={15} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <img src={data.image} />
+            <div className={styles.imagecardtext}>
+                <h4>
+                    {data.title}
+                </h4>
+                <p>
+                    {data.description}
+                </p>
+            </div> */}
+        </div>
+    )
+}
+
+export default function Resources({ news, meta, allCategories, allResources }) {
+    const [selectedCategories, setSelectedCategories] = useState([]);
+
+    const toggleCategory = (category) => {
+        if (selectedCategories.includes(category)) {
+            setSelectedCategories(selectedCategories.filter((each) => each != category));
+        } else {
+            setSelectedCategories([...selectedCategories, category]);
+        }
+    }
+
+    const actualSelectedCategories = selectedCategories.length > 0 ? selectedCategories : allCategories;
+    const filteredResources = allResources.filter((each) => {
+        return each.tags.map(every => every.text).some((eachCategory) => actualSelectedCategories.includes(eachCategory));
+    });
+
+    return (
+        <RootLayout>
+            <Container className={styles.bigblue} fluid>
+                <Container>
+                    <center>
+                        <div className="py-5">
+                            <YellowPill text={meta?.super_title} />
+                            <h2 className={`mt-3 ${styles.herotext}`}>
+                                {meta?.title}
+                            </h2>
+                            <p className={styles.herotext}>
+                                {meta?.description}
+                            </p>
+                            <div className="mt-4 mb-3">
+                                {allCategories.map((each, index) => {
+                                    return (
+                                        <div onClick={() => toggleCategory(each)} key={index} className={`me-3 mb-3 ${styles.category} ${selectedCategories.includes(each) ? styles.selected : ""}`} onClick={() => toggleCategory(each)}>
+                                            {each}
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </center>
+                </Container>
+            </Container>
+            <Container>
+                <div className="mt-5 mb-4">
+                    <h3>
+                        {meta.title_2}
+                    </h3>
+                    <p>
+                        {meta.description_2}
+                    </p>
+                </div>
+                <hr></hr>
+                <Row className="pt-4 pb-5">
+                    {filteredResources.map((each, index) => {
+                        return (
+                            <Col lg={4} md={6} key={index}>
+                                <ImageCard data={each} />
+                            </Col>
+                        )
+                    })}
+                </Row>
+            </Container>
+            <Newsletterform data={news} />
+        </RootLayout>)
+}
+
+export async function getServerSideProps(context) {
+    // add cache headers
+    context.res.setHeader('Cache-Control', CacheHeaders);
+
+    const language = context.locale;
+
+    const resourcesMetaPath = "resources-meta";
+    const meta = await getDataFromPath(resourcesMetaPath, language);
+    const resources = await getAllResources(language);
+    const allCategories = await getAllResourceCategories(language);
+    const news = await getNewsletterData(language);
+
+    return {
+        props: {
+            meta: meta?.data?.attributes,
+            allCategories: allCategories,
+            allResources: resources,
+            news: news
+        }
+    }
+}
---
+++ src/utils/resources.js
@@ -0,0 +1,67 @@
+import { getDataFromPath } from "./api_calls";
+
+var allResources = {
+  en: [],
+  ta: [],
+};
+
+var lastUpdated = new Date(0);
+
+export const getAllResourcesFromStrapi = async (language) => {
+  const maxPageSize = 100;
+  const path = "resources?populate=deep&pagination[pageSize]=" + maxPageSize;
+
+  const rawData = await getDataFromPath(path, language);
+
+  // if more pages are there, get them
+  const totalPages = rawData?.meta?.pagination?.total;
+  const promises = [];
+
+  for (let i = 2; i <= totalPages; i++) {
+    const newPath = path + "&pagination[page]=" + i;
+    promises.push(getDataFromPath(newPath, language));
+  }
+  const allData = await Promise.all(promises);
+  allData.forEach((each) => {
+    rawData.data = rawData.data.concat(each.data);
+  });
+  return rawData?.data?.map((each) => each.attributes);
+};
+
+export const getAllResources = async (language) => {
+  // if cache is empty, get all resources and return
+  if (allResources[language].length === 0) {
+    console.log("Cache is empty");
+    allResources[language] = await getAllResourcesFromStrapi(language);
+    lastUpdated = new Date();
+    return allResources[language];
+  }
+
+  // if cache is not empty, check if cache is stale
+  const now = new Date();
+  const staleTime = 1000 * 60 * 2; // 2 mins
+  if (now - lastUpdated > staleTime) {
+    // cache is stale
+    console.log("Cache is stale");
+    allResources[language] = await getAllResourcesFromStrapi(language);
+    lastUpdated = now;
+    return allResources[language];
+  }
+
+  // cache is not stale
+  console.log("Cache is not stale");
+  return allResources[language];
+};
+
+// export const getAllCategories =
+
+// const allRes = await getAllResources("en");
+// console.log(allRes);
+
+export const getAllResourceCategories = async (language) => [
+  ...new Set(
+    (await getAllResources(language)).flatMap((each) =>
+      each.tags.map((every) => every.text)
+    )
+  ),
+];
