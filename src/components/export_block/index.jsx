8eae95a74f38a2fa77a9e7b815e10ecda79f3518
Sooryakiran <sooryakiranpallikulathil@gmail.com>
2025-01-11 12:13:16 +0530

    export promotions

---
+++ src/components/export_block/index.jsx
@@ -0,0 +1,121 @@
+import { Row, Col, Container } from 'react-bootstrap'
+import styles from './export.module.css'
+import { useState } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+const strings = {
+    phone: {
+        en: 'Phone:',
+        ta: 'தொலைபேசி:'
+    },
+    more: {
+        en: 'Load More',
+        ta: 'மேலும் ஏற்றுக'
+    }
+}
+
+const Promoter = ({ data }) => {
+    const { locale } = useRouter();
+    return (<div className={styles.whitebox}>
+        <div className={styles.yellowbox}>
+            <div className='d-flex mb-0'>
+                <div className="my-auto">
+                    <Image className={styles.promoperson} src="/promo_person.svg" height={22} width={22} />
+                </div>
+                <div className="my-auto ms-2">
+                    <h6 className='mb-0'>
+                        {data.name}
+                    </h6>
+                </div>
+            </div>
+            <div className={styles.roletext}>
+                {data.role}
+            </div>
+        </div>
+        <div className='d-flex mt-2'>
+            <div className="my-auto">
+                <Image src="/promo_phone.svg" height={16} width={16} />
+            </div>
+            <div className="my-auto ms-2">
+                <div className={styles.smalltext}>
+                    {strings.phone[locale]} {data.phone}
+                </div>
+            </div>
+        </div>
+        <div className='d-flex mt-0'>
+            <div className="my-auto">
+                <Image src="/promo_location.svg" height={16} width={16} />
+            </div>
+            <div className="my-auto ms-2">
+                <div className={styles.smalltext}>
+                    {data.location}
+                </div>
+            </div>
+        </div>
+        <div className='d-flex mt-0'>
+            <div className="my-auto">
+                <Image src="/promo_email.svg" height={16} width={16} />
+            </div>
+            <div className="my-auto ms-2">
+                <div className={styles.smalltext}>
+                    {data.email}
+                </div>
+            </div>
+        </div>
+    </div>)
+}
+
+
+export default function ExportBlock({ data, header }) {
+    const maxItems = 9;
+    const [loaded, setLoaded] = useState(maxItems);
+
+    const loadMore = () => {
+        setLoaded(loaded + maxItems);
+    }
+
+    const { locale } = useRouter();
+
+
+    const hasMore = data.length > loaded;
+    const sortedData = data.sort((a, b) => {
+        return a.order_id - b.order_id;
+    })
+
+    const loadedData = sortedData.slice(0, loaded);
+
+
+    return (
+        <Container className="my-5">
+            <div className="d-flex">
+                <div className="my-auto">
+                    <h3>
+                        {header.heading}
+                    </h3>
+                </div>
+                <div className="ms-auto my-auto">
+                    {/* FILTER */}
+                </div>
+            </div>
+            <div className='mt-2'>
+                {header.description}
+            </div>
+            <Row className='mt-4 mb-2'>
+                {loadedData.map((each, index) => {
+                    return (<Col md={6} lg={4} xl={3} key={index}>
+                        <Promoter data={each} />
+                    </Col>)
+                })}
+            </Row>
+
+            <center>
+                {hasMore && <div onClick={loadMore} className={styles.morebutton}>
+                    {strings.more[locale]}
+                </div>}
+            </center>
+
+        </Container>
+    )
+}
+
--- src/pages/export-promotions/index.jsx
+++ src/pages/export-promotions/index.jsx
@@ -12,42 +12,41 @@ import MiniResources from "../../components/miniresources";
 import FaqComponent from "../../components/faqcomponent";
 import Newsletterform from "../../components/newsletterform";
 import BrandingGallery from "../../components/branding_gallery";
-import { getAllPackers } from "../../utils/packers";
+import { getAllPromoters } from "../../utils/export_promoters";
 import Packers from "../../components/packers";
+import ExportBlock from "../../components/export_block";
 
-export default function BrandingAndPackaging({ news, data, packers }) {
+export default function ExportPromotions({ data, news, promoters }) {
+    console.log(data)
     return (
         <RootLayout>
             <Breadcrumps items={data.breadcrumps} />
-            <YellowBlobHero hero={data.hero} hero_imgs={data.hero_imgs} />
+            <YellowBlobHero hero={data.hero} hero_imgs={data.heroimg} />
             <PartnersSection heading={data.partners_title} data={data.partners} />
             <ThreeChannel data={data.threechannel} />
-            <BrandingGallery data={data.section_3} download={data.section_3_download} images={data.section_3_images} />
-            <Packers data={packers} title={data.sectin_4_title} />
+            <ExportBlock data={promoters} header={data.section_3} />
             <UpcomingEvents data={data.events.events.data.map(each => each.attributes)} meta={data.events} />
             <RecentBlogsGrid blogs={data.blogs} />
             <Testimonials data={data.testimonials} />
             <MiniResources data={data.resources} />
             <FaqComponent data={data.faq} />
             <Newsletterform data={news} />
-        </RootLayout>
-    )
+        </RootLayout>)
 }
 
 export async function getServerSideProps(context) {
     context.res.setHeader('Cache-Control', CacheHeaders);
 
-    const path = "branding-and-packaging?&populate=deep";
+    const path = "export-promotions?&populate=deep";
     const language = context.locale;
     const data = await getDataFromPath(path, language);
     const news = await getNewsletterData(language);
-
-    const packers = await getAllPackers(language);
+    const promoters = await getAllPromoters(language);
     return {
         props: {
             news: news,
-            packers: packers,
-            data: data.data.attributes
+            data: data.data.attributes,
+            promoters: promoters
         }
     }
 }
