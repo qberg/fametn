2cc92abb48fe595279de5dbbb0d0d08c370389ec
Sooryakiran <sooryakiranpallikulathil@gmail.com>
2025-02-12 22:58:03 +0530

    added google anals and ms clarity

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
 import styles from './layout.module.css';
-import { JSONData } from '@/utils/definitions';
-import Head from 'next/head';
 import Seo from '../seo';
+import GoogleAnalytics from '../googleanalytics';
+import MicrosoftClarity from '../microsoft_clarity';
+
 export default function RootLayout({
   children,
   data,
@@ -16,6 +17,9 @@ export default function RootLayout({
       <Navbar data={data} />
       <div className={styles.fillVertical}>{children}</div>
       <Footer data={data} />
+
+      <GoogleAnalytics />
+      <MicrosoftClarity />
     </>
   );
 }
