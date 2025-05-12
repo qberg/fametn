8df50f06b21d9f58265db19d00f29bcbb2184718
qberg <qbergnoe@gmail.com>
2025-05-12 20:48:17 +0530

    added regional officials tab of content page

--- src/components/ui/contact-tabs/index.jsx
+++ src/components/ui/contact-tabs/index.jsx
@@ -2,7 +2,17 @@
 import { Col, Container, Row } from "react-bootstrap";
 import styles from "./styles.module.css";
 import { useState } from "react";
-import { MapPin, Phone, SquareArrowOutUpRight } from "lucide-react";
+import {
+  Globe,
+  Mail,
+  MapPin,
+  Minus,
+  Phone,
+  Plus,
+  Smartphone,
+  SquareArrowOutUpRight,
+  User,
+} from "lucide-react";
 import { ContactArrowButton } from "@/components/ui/contact-arrow-button";
 import dynamic from "next/dynamic";
 
@@ -62,7 +72,7 @@ const ContactTabs = ({ fametn, icdic, officials }) => {
                 onKeyDown={(e) => e.key === "Enter" && handleTabClick(index)}
                 tabIndex={0}
               >
-                <h3 className={styles.triggerText}>{trigger.text}</h3>
+                <h4 className={styles.triggerText}>{trigger.text}</h4>
               </div>
             </Col>
           ))}
@@ -75,40 +85,150 @@ const ContactTabs = ({ fametn, icdic, officials }) => {
           <OrgContactCard {...displayData()} />
         )}
 
-        {selectedTab === 2 && (
-          <pre>{JSON.stringify(displayData(), null, 2)}</pre>
-        )}
+        {selectedTab === 2 && <GMContactCard data={displayData()} />}
       </Container>
     </section>
+  );
+};
+
+const GMContactCard = ({ data }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const toggleItem = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  return (
+    <Row className={`${styles.gmContactContainer}`}>
+      {data.map((gmData, index) => (
+        <Col
+          data-aos="fade-up"
+          data-aos-delay={index * 100}
+          key={index}
+          xs={12}
+          md={6}
+          className={styles.gmCol}
+        >
+          <div className={styles.accWrapper}>
+            {/* Trigger */}
+            <div
+              className={styles.accTrigger}
+              role="button"
+              onClick={() => toggleItem(index)}
+            >
+              <div>{`${gmData.post}, ${gmData.district}`}</div>
+
+              <div>{activeIndex === index ? <Minus /> : <Plus />}</div>
+            </div>
+
+            {/* Content */}
+            <div
+              className={`${styles.accContentWrapper} ${activeIndex === index ? styles.accActive : ""} `}
+            >
+              <div className={styles.accContent}>
+                <div className={styles.accItem}>
+                  <div>
+                    <User />
+                  </div>
+                  <div>{gmData.name}</div>
+                </div>
+
+                <div className={styles.accItem}>
+                  <div>
+                    <MapPin />
+                  </div>
+                  <div>
+                    {gmData.address.split(",").map((part, idx) => (
+                      <div key={idx}>{part.trim()}</div>
+                    ))}
+                  </div>
+                </div>
+
+                <div className={styles.accItem}>
+                  <div>
+                    <Mail />
+                  </div>
+                  <a href={`mailto:${gmData.mail}`}>
+                    <div className={styles.gmAnchor}>{gmData.mail}</div>
+                  </a>
+                </div>
+
+                <div className={styles.accItem}>
+                  <div>
+                    <Phone />
+                  </div>
+                  <div>{gmData.tel}</div>
+                </div>
+
+                <div className={styles.accItem}>
+                  <div>
+                    <Smartphone />
+                  </div>
+                  <div>{gmData.mobile}</div>
+                </div>
+
+                <div className={styles.accItem}>
+                  <div>
+                    <Globe />
+                  </div>
+                  <a
+                    href={gmData.website}
+                    target="_self"
+                    rel="noopener noreferrer"
+                    aria-label={`Visit website: ${gmData.website}`}
+                  >
+                    <div className={styles.gmAnchor}>
+                      {gmData.website.replace(/(^\w+:|^)\/\/(www\.)?/, "")}
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Col>
+      ))}
+    </Row>
   );
 };
 
 const OrgContactCard = ({ name, address, tel, mail, website }) => {
   const location = [13.009730416701956, 80.21024319642622];
   return (
-    <Row className={styles.orgContactCard}>
+    <Row data-aos="fade-up" className={styles.orgContactCard}>
       <Col xs={12} md={6} className={styles.orgCol}>
-        <h5>{name}</h5>
+        <h5 data-aos="fade-up" data-aos-delay={40}>
+          {name}
+        </h5>
 
-        <div className={styles.orgCardItem}>
+        <div
+          data-aos="fade-up"
+          data-aos-delay={80}
+          className={styles.orgCardItem}
+        >
           <div>
             <MapPin />
           </div>
           <p>{address}</p>
         </div>
 
-        <div className={styles.orgCardItem}>
+        <div
+          data-aos="fade-up"
+          data-aos-delay={120}
+          className={styles.orgCardItem}
+        >
           <div>
             <Phone />
           </div>
           <p>{tel}</p>
         </div>
 
-        <div className={styles.orgMail}>
+        <div data-aos="fade-up" data-aos-delay={160} className={styles.orgMail}>
           <ContactArrowButton text="Send Mail" link={`mailto:${mail}`} />
         </div>
 
         <a
+          data-aos="fade-up"
+          data-aos-delay={200}
           href={website}
           target="_self"
           rel="noopener noreferrer"
