556cd73423d6438e2280ba5db392778fc93462eb
qberg <qbergnoe@gmail.com>
2025-05-17 22:33:33 +0530

    done minimal resources block component

---
+++ src/components/ui/minimal-events/index.jsx
@@ -0,0 +1,135 @@
+import { Col, Container, Row } from "react-bootstrap";
+import styles from "./styles.module.css";
+import YellowArrowButton from "../../yellow_arrow_button";
+import { Calendar, ExternalLink, MapPin } from "lucide-react";
+
+const MinimalEvents = ({ events, title, description }) => {
+  return (
+    <section className="margin">
+      <Container>
+        <Row className={styles.contentRow}>
+          <Col
+            xs={{ span: 12, order: 1 }}
+            md={{ span: 5, order: 1 }}
+            data-aos="fade-up"
+          >
+            <div className={styles.contentFlex} data-aos="fade-up">
+              <h2 data-aos="fade-up">{title}</h2>
+
+              <p data-aos="fade-up">{description}</p>
+
+              <div className={styles.ctaDeskFlex} data-aos="fade-up">
+                <YellowArrowButton text="View More" link="/events" />
+              </div>
+            </div>
+          </Col>
+
+          {/*Events */}
+          <Col
+            xs={{ span: 12, order: 2 }}
+            md={{ span: 6, order: 2, offset: 1 }}
+            className={styles.eventCol}
+            data-aos="fade-up"
+          >
+            <div className={styles.eventsFlex} data-aos="fade-up">
+              {events.map((event, index) => (
+                <EventBookMarkCard key={index} {...event} index={index} />
+              ))}
+            </div>
+          </Col>
+
+          <Col
+            xs={{ span: 12, order: 3 }}
+            md={0}
+            className={styles.ctaMobileFlex}
+          >
+            <YellowArrowButton text="View More" link="/events" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+const EventBookMarkCard = ({
+  index,
+  start_date,
+  end_date,
+  title,
+  description,
+  location,
+  registration_link,
+}) => {
+  const formatDate = (dateStr) => {
+    const date = new Date(dateStr);
+
+    return {
+      day: date.getDate(),
+      month: date.toLocaleString("default", { month: "long" }),
+      year: date.getFullYear(),
+    };
+  };
+
+  const { day: startDay, month: startMonth, year } = formatDate(start_date);
+  const { day: endDay } = formatDate(end_date);
+
+  const duration = `${startDay}-${endDay} ${startMonth} ${year}`;
+
+  return (
+    <article
+      className={styles.card}
+      data-aos="fade-up"
+      data-aos-delay={index * 50}
+    >
+      <div className={styles.bookmark}>
+        <span className={styles.bookmarkDay} data-aos="fade-up">
+          {startDay}
+        </span>
+        <span className={styles.bookmarkMonth} data-aos="fade-up">
+          {startMonth}
+        </span>
+      </div>
+
+      <div className={styles.eventContent}>
+        <a
+          href={registration_link || "/events"}
+          target="_blank"
+          rel="noopener noreferrer"
+          data-aos="fade-up"
+        >
+          <div className={`${styles.eventTitle} niceTitle`}>
+            <span>{title}</span>
+            <span className={styles.externalLinkIcon}>
+              <ExternalLink size={20} />{" "}
+            </span>
+          </div>
+        </a>
+
+        {description && (
+          <div data-aos="fade-up" className={`${styles.eventDesc} niceDesc`}>
+            {description}
+          </div>
+        )}
+
+        <div className={styles.eventSchedule}>
+          <div className={styles.eventMeta}>
+            <span data-aos="fade-up" className={styles.icon}>
+              <Calendar size={18} />
+            </span>
+
+            <span data-aos="fade-up">{duration}</span>
+          </div>
+
+          <div className={styles.eventMeta}>
+            <span data-aos="fade-up" className={styles.icon}>
+              <MapPin size={18} />
+            </span>
+            <span data-aos="fade-up">{location}</span>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default MinimalEvents;
--- src/pages/quality-and-sustainability/industry-4-0/index.jsx
+++ src/pages/quality-and-sustainability/industry-4-0/index.jsx
@@ -1,27 +1,22 @@
 import RootLayout from "@/components/layout/layout";
 import { CacheHeaders } from "@/utils/definitions";
-import {
-  getDataFromPath,
-  getHeaderFooterData,
-  getNewsletterData,
-} from "@/utils/api_calls";
+import { getDataFromPath, getHeaderFooterData } from "@/utils/api_calls";
 import PartnersSection from "@/components/partners";
-import UpcomingEvents from "@/components/upcoming_events";
 import RecentBlogsGrid from "@/components/recentblogsgrid";
-import Newsletterform from "@/components/newsletterform";
 import ContentHero from "../../../components/ui/content-hero";
+import MinimalEvents from "../../../components/ui/minimal-events";
 
-export default function Industry4PointO({ data, news, headerFooter }) {
+export default function Industry4PointO({ data, headerFooter }) {
   return (
     <RootLayout seo={data.seo} data={headerFooter}>
       <ContentHero {...data.hero} breadcrumps={data.breadcrumps} />
       <PartnersSection heading={data.partner_title} data={data.partners} />
-      <UpcomingEvents
-        data={data.events.events.data.map((each) => each.attributes)}
-        meta={data.events}
+      <MinimalEvents
+        events={data.events.events.data.map((each) => each.attributes)}
+        title={data.events.title_2}
+        description={data.events.description_2}
       />
       <RecentBlogsGrid blogs={data.blogs} />
-      <Newsletterform data={news} />
     </RootLayout>
   );
 }
@@ -32,10 +27,8 @@ export async function getServerSideProps(context) {
   const path = "industry-4-0?&populate=deep";
   const language = context.locale;
   const data = await getDataFromPath(path, language);
-  const news = await getNewsletterData(language);
   return {
     props: {
-      news: news,
       data: data.data.attributes,
       headerFooter: await getHeaderFooterData(language),
     },
