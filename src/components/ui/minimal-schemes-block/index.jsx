ad58531a05ec2abd5280b922cd170b57d6e51f57
qberg <qbergnoe@gmail.com>
2025-05-17 16:04:05 +0530

    added fade up animation to minimal schemes block component

--- src/components/ui/minimal-schemes-block/index.jsx
+++ src/components/ui/minimal-schemes-block/index.jsx
@@ -8,13 +8,15 @@ const MinimalSchemesBlock = ({ header, cta, schemes }) => {
   return (
     <section className="margin">
       <Container>
-        <div className={styles.wrapper}>
+        <div className={styles.wrapper} data-aos="fade-up">
           {header && (
             <Row>
               <Col xs={12}>
                 <center>
                   <h2 data-aos="fade-up">{header.heading}</h2>
-                  <p className={styles.secDesc}>{header.description}</p>
+                  <p data-aos="fade-up" className={styles.secDesc}>
+                    {header.description}
+                  </p>
                 </center>
               </Col>
             </Row>
@@ -27,6 +29,7 @@ const MinimalSchemesBlock = ({ header, cta, schemes }) => {
                 return (
                   <Col key={index} xs={12} md={4} className={styles.schemeCol}>
                     <SpreadColorCard
+                      index={index}
                       icon={icon}
                       title={scheme_name}
                       description={scheme_description}
@@ -43,7 +46,7 @@ const MinimalSchemesBlock = ({ header, cta, schemes }) => {
   );
 };
 
-const SpreadColorCard = ({ icon, title, description, ctaLink }) => {
+const SpreadColorCard = ({ index, icon, title, description, ctaLink }) => {
   const customPath = icon?.data?.attributes?.formats?.small?.url;
   const iconPath = customPath
     ? process.env.NEXT_PUBLIC_IMG_ENDPOINT + customPath
@@ -51,9 +54,13 @@ const SpreadColorCard = ({ icon, title, description, ctaLink }) => {
 
   return (
     <Link href={ctaLink}>
-      <div className={styles.card}>
+      <div
+        className={styles.card}
+        data-aos="fade-up"
+        data-aos-delay={100 * index}
+      >
         <div className={styles.iconWrapper}>
-          <div className={styles.icon}>
+          <div className={styles.icon} data-aos="fade-up">
             <Image
               height={75}
               width={75}
@@ -63,12 +70,17 @@ const SpreadColorCard = ({ icon, title, description, ctaLink }) => {
           </div>
         </div>
 
-        <div className={styles.content}>
-          <div className={styles.title}> {title}</div>
-          <div className={styles.description}>{description}</div>
+        <div className={styles.content} data-aos="fade-up">
+          <div className={styles.title} data-aos="fade-up">
+            {" "}
+            {title}
+          </div>
+          <div className={styles.description} data-aos="fade-up">
+            {description}
+          </div>
         </div>
 
-        <div className={styles.arrow}>
+        <div className={styles.arrow} data-aos="fade-up">
           <ArrowRight size={24} />
         </div>
       </div>
